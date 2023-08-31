a955edc2abc1d7582c046175cdd59e5b53ea1dd4
jovijulian <jovijulian2002@gmail.com>
2023-08-31 23:57:10 +0700

    add endpoint Delete segment

--- app/Http/Controllers/Api/SegmentController.php
+++ app/Http/Controllers/Api/SegmentController.php
@@ -228,8 +228,37 @@ class SegmentController extends Controller
     /**
      * Remove the specified resource from storage.
      */
+    protected function delete($id)
+    {
+
+        $segment = Segment::find($id);
+        if ($segment == null) {
+            throw new \Exception("Segmen tidak ada", 404);
+        }
+        $segment->delete();
+
+        return $segment;
+    }
     public function destroy(string $id)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $this->delete($id);
+            DB::commit();
+            // return
+            return ResponseStd::okNoOutput("Segmen berhasil dihapus.");
+        } catch (\Exception $e) {
+            DB::rollBack();
+            if ($e instanceof ValidationException) {
+                return ResponseStd::validation($e->validator);
+            } else {
+                Log::error($e->getMessage());
+                if ($e instanceof QueryException) {
+                    return ResponseStd::fail(trans('error.global.invalid-query'));
+                } else {
+                    return ResponseStd::fail($e->getMessage());
+                }
+            }
+        }
     }
 }
