9c53102f1f9a7e43887689adbec7c70f04c9a0e0
jovijulian <jovijulian2002@gmail.com>
2023-09-10 14:33:41 +0700

    add endpoint crud user management

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers\Api;
 
+use Carbon\Carbon;
+use App\Models\User;
+use Ramsey\Uuid\Uuid;
+use Illuminate\Http\Request;
+use App\Libraries\ResponseStd;
+use App\Rules\OnlyVerifiedMail;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
-use App\Libraries\ResponseStd;
-use App\Models\User;
-use App\Rules\OnlyVerifiedMail;
-use Carbon\Carbon;
-use Illuminate\Database\QueryException;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Validation\ValidationException;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class UserController extends Controller
@@ -159,5 +160,283 @@ class UserController extends Controller
         $user->save();
 
         return $user;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        try {
+            $search_term = $request->input('search');
+            $limit = $request->has('limit') ? $request->input('limit') : 10;
+            $sort = $request->has('sort') ? $request->input('sort') : 'created_at';
+            $order = $request->has('order') ? $request->input('order') : 'DESC';
+            $conditions = '1 = 1';
+            // Jika dari frontend memaksa limit besar.
+            if ($limit > 10) {
+                $limit = 10;
+            }
+
+            if (!empty($search_term)) {
+                $conditions .= " AND users.fullname LIKE '%$search_term%'";
+            }
+
+            $paginate = User::query()->select(['users.*'])->where('isAdmin', 0)
+                ->whereRaw($conditions)
+                ->orderBy($sort, $order)
+                ->paginate($limit);
+
+            $countAll = User::query()
+                ->count();
+
+            // paging response.
+            $response = UserResource::collection($paginate);
+            return ResponseStd::pagedFrom($response, $paginate, $countAll);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            if ($e instanceof ValidationException) {
+                return ResponseStd::validation($e->validator);
+            } else {
+                Log::error($e->getMessage());
+                if ($e instanceof QueryException) {
+                    return ResponseStd::fail(trans('error.global.invalid-query'));
+                } else {
+                    return ResponseStd::fail($e->getMessage(), $e->getCode());
+                }
+            }
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+
+    protected function validateCreate(array $data)
+    {
+        $arrayValidator = [
+            'fullname' => ['required', 'string', 'min:3'],
+            'email' => [
+                'required',
+                'email',
+                'min:3',
+                'max:80',
+                'unique:users,email,NULL,id',
+                new OnlyVerifiedMail
+            ],
+            'password' => ['required', 'string', 'min:3'],
+            'phone_number' => [
+                'required',
+                'min:9',
+                'max:15',
+                'unique:users,phone_number,NULL,id'
+            ],
+            'group_id' => ['required'],
+            'group_name' => ['required', 'string', 'between:1,100'],
+        ];
+
+        return Validator::make($data, $arrayValidator);
+    }
+    protected function create(array $data, Request $request)
+    {
+
+        $timeNow = Carbon::now();
+        $user = new User();
+        $user->id = Uuid::uuid4()->toString();
+        $user->email = $data['email'];
+        $user->password = Hash::make($data['password']);
+        $user->fullname = $data['fullname'];
+        $user->phone_number = $data['phone_number'];
+        $user->address = $data['address'];
+        $user->birthdate = $data['birthdate'];
+        $user->group_id = $data['group_id'];
+        $user->group_name = $data['group_name'];
+        $user->isAdmin = 0;
+        $user->email_verified_at = $timeNow;
+        $user->created_at = $timeNow;
+        $user->created_by = auth()->user()->fullname;
+        $user->updated_by = null;
+        $user->save();
+        return $user;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        DB::beginTransaction();
+        try {
+            $validate = $this->validateCreate($request->all());
+            if ($validate->fails()) {
+                throw new ValidationException($validate);
+            }
+            $model = $this->create($request->all(), $request);
+            DB::commit();
+
+            // return
+            $single = new UserResource($model);
+            return ResponseStd::okSingle($single);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            if ($e instanceof ValidationException) {
+                return ResponseStd::validation($e->validator);
+            } else {
+                Log::error($e->getMessage());
+                if ($e instanceof QueryException) {
+                    return ResponseStd::fail(trans('error.global.invalid-query'));
+                } else {
+                    return ResponseStd::fail($e->getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $model = User::query()->find($id);
+            if (empty($model)) {
+                throw new BadRequestHttpException("User tidak ada");
+            }
+            $single = new UserResource($model);
+            return ResponseStd::okSingle($single);
+        } catch (\Exception $e) {
+            if ($e instanceof ValidationException) {
+                return ResponseStd::validation($e->validator);
+            } else {
+                Log::error(__CLASS__ . ":" . __FUNCTION__ . ' ' . $e->getMessage());
+                if ($e instanceof QueryException) {
+                    return ResponseStd::fail(trans('error.global.invalid-query'));
+                } else {
+                    return ResponseStd::fail($e->getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+
+    protected function validateUpdate(array $data)
+    {
+        $arrayValidator = [
+            'fullname' => ['required', 'string', 'min:3'],
+            'email' => [
+                'required',
+                'email',
+                'min:3',
+                'max:80',
+                new OnlyVerifiedMail
+            ],
+            'password' => ['required', 'string', 'min:3'],
+            'phone_number' => [
+                'required',
+                'min:9',
+                'max:15',
+            ],
+            'group_id' => ['required'],
+            'group_name' => ['required', 'string', 'between:1,100'],
+        ];
+        return Validator::make($data, $arrayValidator);
+    }
+
+    protected function edit($id, array $data, Request $request)
+    {
+        $timeNow = Carbon::now();
+
+        // Find user by id
+        $user = User::find($id);
+
+        if (empty($user)) {
+            throw new \Exception("Invalid user id", 406);
+        }
+        $user->id = $id;
+        $user->email = $data['email'];
+        $user->password = Hash::make($data['password']);
+        $user->fullname = $data['fullname'];
+        $user->phone_number = $data['phone_number'];
+        $user->address = $data['address'];
+        $user->birthdate = $data['birthdate'];
+        $user->group_id = $data['group_id'];
+        $user->group_name = $data['group_name'];
+        $user->updated_at = $timeNow;
+        $user->updated_by = auth()->user()->fullname;
+        //Save
+        $user->save();
+
+        return $user;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update($id, Request $request)
+    {
+        DB::beginTransaction();
+        try {
+            $validate = $this->validateUpdate($request->all());
+            if ($validate->fails()) {
+                throw new ValidationException($validate);
+            }
+            $data = $this->edit($id, $request->all(), $request);
+
+            DB::commit();
+
+            // return.
+            $single = new UserResource($data);
+            return ResponseStd::okSingle($single);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            if ($e instanceof ValidationException) {
+                return ResponseStd::validation($e->validator);
+            } else {
+                Log::error($e->getMessage());
+                if ($e instanceof QueryException) {
+                    return ResponseStd::fail(trans('error.global.invalid-query'));
+                } else {
+                    return ResponseStd::fail($e->getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    protected function delete($id)
+    {
+
+        $user = User::find($id);
+
+        $user->forceDelete();
+
+        return $user;
+    }
+    public function destroy(string $id)
+    {
+        DB::beginTransaction();
+        try {
+            $this->delete($id);
+            DB::commit();
+            // return
+            return ResponseStd::okNoOutput("User berhasil dihapus.");
+        } catch (\Exception $e) {
+            DB::rollBack();
+            if ($e instanceof ValidationException) {
+                return ResponseStd::validation($e->validator);
+            } else {
+                Log::error($e->getMessage());
+                if ($e instanceof QueryException) {
+                    return ResponseStd::fail(trans('error.global.invalid-query'));
+                } else {
+                    return ResponseStd::fail($e->getMessage());
+                }
+            }
+        }
     }
 }
