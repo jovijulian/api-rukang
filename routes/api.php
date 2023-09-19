a5961658eb9383c0e9e0dfda03eb764b34f591e7
jovijulian <jovijulian2002@gmail.com>
2023-09-20 00:14:25 +0700

    add function insert multiple image with same status

--- routes/api.php
+++ routes/api.php
@@ -107,6 +107,7 @@ Route::prefix('v1')->group(function () {
             Route::post('/update-status/{id}', 'setStatusLogProduct')->middleware(['auth:api', 'check_admin:1,2,3,4']);
             Route::post('/datatable', 'datatable')->middleware(['auth:api', 'check_admin:1,2,3,4,5']);
             Route::put('/edit-location/{id}', 'editLocation')->middleware(['auth:api', 'check_admin:1,2,3,4']);
+            Route::post('/multiple-image-status/{id}', 'addMultipleImagesStatus')->middleware(['auth:api', 'check_admin:1,2,3,4']);
         });
     });
     Route::prefix('shipping')->group(function () {
